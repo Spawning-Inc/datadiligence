80f4f596a04a2648a5c797231a67905c8ed64164
Nick Padgett <nickpadge@gmail.com>
2023-07-03 12:50:08 -0400

    Added TDMRep http header rule. Added the rule to the http and postprocessing evaluators. Still need to add automated tests.

--- src/datadiligence/evaluators/http.py
+++ src/datadiligence/evaluators/http.py
@@ -3,7 +3,7 @@ This module contains the HttpEvaluator class.
 """
 
 from .base import Evaluator
-from ..rules import XRobotsTagHeader
+from ..rules import XRobotsTagHeader, TDMRepHeader
 
 
 class HttpEvaluator(Evaluator):
@@ -12,13 +12,16 @@ class HttpEvaluator(Evaluator):
     """
     name = "http"
 
-    def __init__(self, user_agent=None, respect_robots=True):
+    def __init__(self, user_agent=None, respect_robots=True, respect_tdmrep=True):
         """Load the default rules.
 
         Args:
             user_agent (str): The user agent to pass on to the rules.
             respect_robots (bool): Whether to respect the X-Robots-Tag header.
+            respect_tdmrep (bool): Whether to respect the TDMRep header.
         """
         super().__init__()
         if respect_robots:
             self.rules.append(XRobotsTagHeader(user_agent))
+        if respect_tdmrep:
+            self.rules.append(TDMRepHeader())
--- src/datadiligence/rules/base.py
+++ src/datadiligence/rules/base.py
@@ -1,3 +1,8 @@
+import http.client
+import requests.structures
+from ..exceptions import HttpUnknownHeaderObject, HttpUnknownResponseObject
+from ..utils import get_url
+
 """
 This module contains the base rule classes.
 """
@@ -40,3 +45,62 @@ class HttpRule(Rule):
         """
         super().__init__()
         self.user_agent = user_agent
+
+    def get_header_value(self, headers, header_name):
+        """
+        Handle the headers object to get the header value.
+
+        Args:
+            headers (dict|http.client.HTTPMessage|CaseInsensitiveDict): The headers object.
+            header_name (str): The header name.
+
+        Returns:
+            str: The header value.
+        """
+        if type(headers) == dict or type(headers) == requests.structures.CaseInsensitiveDict:
+            header_value = headers.get(header_name, "")
+        elif type(headers) == list and len(headers) > 0 and type(headers[0]) == tuple:
+            header_value = dict(headers).get(header_name, "")
+        elif type(headers) == http.client.HTTPMessage:
+            header_value = headers.get(header_name, "")
+        else:
+            raise HttpUnknownHeaderObject()
+
+        return header_value
+
+    def is_ready(self):
+        """
+        These rules should always be ready.
+        """
+        return True
+
+    def _handle_url(self, url):
+        """
+        If given a raw URL, submit a request to get the image.
+
+        Args:
+            url (str): The URL of the resource.
+
+        Returns:
+            requests.Response: Response object.
+        """
+        return get_url(url, user_agent=self.user_agent)
+
+    def get_header_value_from_response(self, response, header_name):
+        """
+        Handle the response object to get the header value.
+
+        Args:
+            response (http.client.HTTPResponse|requests.Response): The response object.
+            header_name (str): The header name.
+
+        Returns:
+            str: The header value.
+        """
+        if type(response) == http.client.HTTPResponse:
+            header_value = response.getheader(header_name, "")
+        elif type(response) == requests.Response:
+            header_value = response.headers.get(header_name, "")
+        else:
+            raise HttpUnknownResponseObject()
+        return header_value
